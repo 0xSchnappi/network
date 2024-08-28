61a66bcf655f99a2b4d8450e8a8a79b0e073ca09
0xSchnappi <952768182@qq.com>
2024-08-28 11:35:42 +0800

    [网络编程实战]：TCP读写数据
    TCP客户端写入数据，服务端读取数据

--- tcp-client.c
+++ tcp-client.c
@@ -2,7 +2,7 @@
  * @Author: 0xSchnappi 952768182@qq.com
  * @Date: 2024-08-27 09:32:37
  * @LastEditors: 0xSchnappi 952768182@qq.com
- * @LastEditTime: 2024-08-28 10:16:24
+ * @LastEditTime: 2024-08-28 11:26:03
  * @FilePath: /network/tcp-client.c
  * @Description: tcp 客户端
  *
@@ -13,6 +13,8 @@
 #include <errno.h>
 #include <netinet/in.h> // sockaddr_in 结构体
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h> // close函数
 
@@ -41,4 +43,34 @@ void main() {
     perror("Connection");
     close(socket_fd);
   }
+
+  send_data(stdin, socket_fd);
+}
+
+#define MESSAGE_SIZE 10240000
+void send_data(FILE *fp, int sockfd) {
+  char *query;
+  query = malloc(MESSAGE_SIZE + 1);
+  for (size_t i = 0; i < MESSAGE_SIZE; i++) {
+    query[i] = 'a' + i % 26;
+  }
+  query[MESSAGE_SIZE] = '\0';
+
+  const char *cp;
+  cp = query;
+  int remaining = strlen(query);
+
+  while (remaining) {
+    int n_written = send(sockfd, cp, remaining, 0);
+    fprintf(stdout, "Send into buffer %ld \n", n_written);
+    if (n_written <= 0) {
+      perror("Send");
+      return;
+    }
+    remaining -= n_written;
+    cp += n_written;
+  }
+
+  free(query);
+  return;
 }
--- tcp-server.c
+++ tcp-server.c
@@ -2,16 +2,19 @@
  * @Author: 0xSchnappi 952768182@qq.com
  * @Date: 2024-08-27 09:32:44
  * @LastEditors: 0xSchnappi 952768182@qq.com
- * @LastEditTime: 2024-08-28 10:17:04
+ * @LastEditTime: 2024-08-28 11:30:05
  * @FilePath: /network/tcp-server.c
  * @Description: tcp 服务端
  *
  * Copyright (c) 2024 by github.com/0xSchnappi, All Rights Reserved.
  */
 
+#include <errno.h>
 #include <netinet/in.h> // sockaddr_in
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h> // bzero函数
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -46,14 +49,67 @@ void main() {
   }
 
   /* 循环处理用户请求 */
-  struct sockaddr_in reqname = {0};
+  struct sockaddr_in reqname;
   int nsize;
-  nsize = sizeof(reqname);
-  int accept_fd = accept(socket_fd, (struct sockaddr *)&reqname, &nsize);
+  int accept_fd;
+  while (true) {
 
-  if (accept_fd == -1) {
-    perror("Accept");
-    close(socket_fd);
-    return -1;
+    nsize = sizeof(reqname);
+    bzero(&reqname, nsize);
+    accept_fd = accept(socket_fd, (struct sockaddr *)&reqname, &nsize);
+
+    if (accept_fd == -1) {
+      perror("Accept");
+      close(socket_fd);
+      return -1;
+    }
+    read_data(accept_fd);
+    close(accept_fd);
+  }
+  const char *message = "Hello, Client!";
+  send(accept, message, strlen(message), 0);
+}
+
+/* 从 socketfd 描述字中读取 size 个字节 */
+size_t readn(int fd, void *vptr, size_t size) {
+  size_t nleft;
+  ssize_t nread;
+  char *ptr;
+
+  ptr = vptr;
+  nleft = size;
+
+  while (nleft > 0) {
+    if ((nread = read(fd, ptr, nleft)) < 0) {
+      if (errno == EINTR) // 呼叫在传输任何数据之前被信号中断读取
+        nread =
+            0; /* 这里需要再次调用read。表示是非阻塞I/O的情况下，没有数据可以读，需要继续调用read
+                */
+      else
+        return -1;
+    } else if (nread == 0) {
+      break; /* EOF(End of File)表示套接字关闭。表述读到对方发出的FIN包 */
+    }
+
+    nleft -= nread; // 需要读取的字符减少
+    ptr += nread;   // 缓存指针往下移动
+  }
+  return size - nleft; /* 返回的是实际读取的字节数 */
+}
+
+void read_data(int sockfd) {
+  ssize_t n;
+  char buf[1024] = {0};
+
+  int time = 0;
+  for (;;) {
+    fprintf(stdout, "Block in read\n");
+    if ((n = readn(sockfd, buf, 1024)) == 0) {
+      return;
+    }
+    time++;
+    // fprintf(stderr, "%s\n", buf);
+    fprintf(stdout, "1K read for %d\n", time);
+    usleep(1000);
   }
 }
